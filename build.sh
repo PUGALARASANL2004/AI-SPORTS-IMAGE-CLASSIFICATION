f44af34592258cf54d9957400766db526f6273f6
PUGALARASANL2004 <PUGALARASAN141@GMAIL.COM>
2026-02-12 18:46:17 +0530

    Fix build error by upgrading setuptools and adding it to requirements

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Upgrade build tools
+# Upgrade build tools to ensure we can install modern packages
 pip install --upgrade pip setuptools wheel
+
+# Install dependencies needed for building some packages
+pip install --upgrade "setuptools>=70.0.0" "wheel>=0.40.0"
 
 # Install dependencies
 pip install -r requirements.txt
